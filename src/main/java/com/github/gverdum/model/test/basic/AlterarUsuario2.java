b90b2d78479351ffb08a26002179ac011894f2c5
gianverdum <gian.verdum@gmail.com>
2025-04-28 16:54:03 -0300

    Getting to know unmanaged and managed object state

---
+++ src/main/java/com/github/gverdum/model/test/basic/AlterarUsuario2.java
@@ -0,0 +1,26 @@
+package com.github.gverdum.model.test.basic;
+
+import com.github.gverdum.model.basic.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class AlterarUsuario2 {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        Usuario usuario = em.find(Usuario.class, 7L);
+        usuario.setNome("Leonardo Alterado");
+
+        em.getTransaction().begin();
+//        em.merge(usuario);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}
